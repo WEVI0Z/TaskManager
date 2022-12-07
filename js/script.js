9311172a3986a1e9fe30f9a21cfc1ea528bfa695
Wevioz <wevioz@mail.ru>
2022-12-07 09:59:46 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+const MENU_MARKUP = `
+    <h1 class="control__title">TASKMANAGER</h1>
+    <section class="control__btn-wrap">
+    <input
+        type="radio"
+        name="control"
+        id="control__new-task"
+        class="control__input visually-hidden"
+    />
+    <label for="control__new-task" class="control__label control__label--new-task"
+        >+ ADD NEW TASK</label
+    >
+    <input
+        type="radio"
+        name="control"
+        id="control__task"
+        class="control__input visually-hidden"
+        checked
+    />
+    <label for="control__task" class="control__label">TASKS</label>
+    <input
+        type="radio"
+        name="control"
+        id="control__statistic"
+        class="control__input visually-hidden"
+    />
+    <label for="control__statistic" class="control__label"
+        >STATISTICS</label
+    >
+    </section>
+`;
+
+
+function insertMarkup(container, element) {
+    container.innerHTML = '';
+    container.insertAdjacentHTML('afterBegin', element);
+}
+
+insertMarkup(document.querySelector('.main__control'), MENU_MARKUP);
